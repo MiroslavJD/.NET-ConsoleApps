b060da59eee452888df4f73cfb85090dd6c06ceb
Мирослав Димитров <33944218+MiroslavJD@users.noreply.github.com>
2018-08-23 22:02:17 +0300

    Add files via upload

---
+++ BubbleSortingStringOperation.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiroslavD
+{   // Strings//
+   
+    public class Program
+    {
+        // Write a program in C# Sharp to read a string through the keyboard and sort it using bubble sort.
+            //Test Data : 
+            //Input number of strings :3 
+            //Input 3 strings below : 
+            //abcd 
+            //zxcv 
+            //mnop 
+        public static void  Main(string[] args)
+        {
+            Console.Write("Enter number of strings: ");
+            int n = Int32.Parse(Console.ReadLine());
+            string[] array = new string[n];
+            string temp;
+            Console.Write("Create the strings: \n");
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.Write("{0} ", i);
+                array[i] = Console.ReadLine();
+            }
+            Console.Write("Show the strings: \n");
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.Write("{0} ", array[i]);
+            }
+            for(int i = 0; i < array.Length; i++)
+            {
+                for(int j = 0; j < array.Length - 1; j++)
+                {
+                    if(array[i].CompareTo(array[j+1]) > 0)
+                    {
+                        temp = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = temp;
+
+                    }
+                }
+            }
+            Console.Write("\nShow the strings sorted: \n");
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.Write("{0} ", array[i]);
+            }
+            Console.WriteLine();
+            
+            
+           
+
+            
+
+        }       
+    }
+}
+ 
+                 
+       
+ 
+
+
+
+ 
+    
+
+        
+
+
