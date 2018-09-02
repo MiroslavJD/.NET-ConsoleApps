8da2455480adeaf637fa3760303c75472ac00979
Мирослав Димитров <33944218+MiroslavJD@users.noreply.github.com>
2018-09-02 18:44:24 +0300

    Add files via upload

---
+++ GraphBFSDFS.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiroslavD
+{
+     class Graph
+     {
+         private int Vertices;
+         private List<int>[] adj;
+         public Graph(int v)
+         {
+            Vertices = v;
+            adj = new List<Int32>[v];
+              for (int i = 0; i < v; i++)
+            {
+                adj[i] = new List<Int32>();
+            }
+        }
+         public void AddEdge(int v, int w)
+         {
+             adj[v].Add(w);
+         }
+         public void BFS(int s)
+         {
+            bool[] visited = new bool[Vertices];
+            Queue<int> queue = new Queue<int>();
+            visited[s] = true;
+            queue.Enqueue(s);
+            while (queue.Count!=0)
+            {
+                s = queue.Dequeue();
+                Console.WriteLine("next->"+s);
+                foreach (Int32 next in adj[s])
+                {
+                    if (!visited[next])
+                    {
+                        visited[next] = true;
+                        queue.Enqueue(next);
+                    }
+                }
+            }
+         }
+         public void DFS(int s)
+         {
+             bool[] visited = new bool[Vertices];
+             
+             Stack<int> stack = new Stack<int>();
+             visited[s] = true;
+             stack.Push(s);
+
+             while (stack.Count != 0)
+             {
+                 s = stack.Pop();
+                 Console.WriteLine("next->" + s);
+                 foreach (int i in adj[s])
+                 {
+                     if (!visited[i])
+                     {
+                         visited[i] = true;
+                         stack.Push(i);
+                     }
+                 }
+             }
+         }
+         public void PrintAdjacecnyMatrix()
+         {
+             for (int i = 0; i < Vertices; i++)
+             {
+                 Console.Write(i + ":[");
+                 string s = "";
+                 foreach (var k in adj[i])
+                 {
+                     s = s + (k + ",");
+                 }
+                 s = s.Substring(0, s.Length - 1);
+                 s = s + "]";
+                 Console.Write(s);
+                 Console.WriteLine();
+             }
+         }
+         public static void Main()
+         {
+            Graph graph = new Graph(4);
+            graph.AddEdge(0,1);
+            graph.AddEdge(0,2);
+            graph.AddEdge(1,2);
+            graph.AddEdge(2,0);
+            graph.AddEdge(2,3);
+            graph.AddEdge(3,3);
+
+            graph.PrintAdjacecnyMatrix();
+ 
+            Console.WriteLine("BFS traversal starting from vertex 2:");
+            graph.BFS(2);
+            Console.WriteLine("DFS traversal starting from vertex 2:");
+            graph.DFS(2);
+        }
+    }
+ }
+
