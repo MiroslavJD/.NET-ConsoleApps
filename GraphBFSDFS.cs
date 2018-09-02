01d85a175dde0676e5c89abd01182d9c2822456a
Мирослав Димитров <33944218+MiroslavJD@users.noreply.github.com>
2018-09-02 18:45:50 +0300

    Update GraphBFSDFS.cs

--- GraphBFSDFS.cs
+++ GraphBFSDFS.cs
@@ -84,20 +84,20 @@ namespace MiroslavD
          }
          public static void Main()
          {
-            Graph graph = new Graph(4);
+            Graph graph = new Graph(5);
             graph.AddEdge(0,1);
-            graph.AddEdge(0,2);
             graph.AddEdge(1,2);
-            graph.AddEdge(2,0);
             graph.AddEdge(2,3);
-            graph.AddEdge(3,3);
+            graph.AddEdge(3,4);
+            graph.AddEdge(4,4);
+            
 
             graph.PrintAdjacecnyMatrix();
  
-            Console.WriteLine("BFS traversal starting from vertex 2:");
-            graph.BFS(2);
-            Console.WriteLine("DFS traversal starting from vertex 2:");
-            graph.DFS(2);
+            Console.WriteLine("BFS traversal starting from vertex 1:");
+            graph.BFS(1);
+            Console.WriteLine("DFS traversal starting from vertex 1:");
+            graph.DFS(1);
         }
     }
  }
