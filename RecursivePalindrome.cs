a9b3a8bb5e65a773723e68d94ffe842779bb04ad
Мирослав Димитров <33944218+MiroslavJD@users.noreply.github.com>
2018-09-17 18:34:32 +0300

    Add files via upload

---
+++ RecursivePalindrome.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiroslavD
+{
+    class RecursivePalindrome
+    {
+        public static bool Palindrome(string str)
+        {
+            if (str.Length <= 2)
+                return true;
+            else
+            {
+                if (str[0] != str[str.Length - 1])
+                {
+                    return false;
+                }
+                else
+                    return Palindrome(str.Substring(str.Length));
+            }
+        }
+        public static void Main()
+        {
+            Console.WriteLine("String: ");
+            string str = Console.ReadLine();
+
+            bool check = Palindrome(str);
+            if (check)
+            {
+                Console.WriteLine("{0} is palindrome ", str);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not palindrome ", str);
+            }
+            Console.WriteLine();
+        }
+    }
+}
