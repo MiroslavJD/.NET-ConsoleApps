685afc7a2112a670768012ddaf96ba4d9947001d
Мирослав Димитров <33944218+MiroslavJD@users.noreply.github.com>
2018-09-23 01:00:05 +0300

    Add files via upload

---
+++ MaxOccuringCharInString.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiroslavD
+{
+    class MaxOccuringCharInString
+    { 
+        public  static string MaxOccuringChar(string str)
+        {
+            int[] array = new int[120];
+            for (int i = str.Length-1; i-- > 0; array[str[i]]++) ;
+            int maxIndex = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if(array[i] > array[maxIndex])
+                {
+                    maxIndex = i;
+                }
+            }
+
+            return (char)maxIndex + " : " + array[maxIndex];
+        }
+
+        public static void Main()
+        {
+            Console.Write("Enter string: ");
+            string str = Console.ReadLine();
+            Console.WriteLine("Max occuring char is {0} ", MaxOccuringChar(str));
+
+        }
+    }
+}
