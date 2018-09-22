ed79ca873f592d902bb75f97aeb6e108bcc49f55
Мирослав Димитров <33944218+MiroslavJD@users.noreply.github.com>
2018-09-22 12:15:46 +0300

    Add files via upload

---
+++ Majorant.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiroslavD
+{
+    class Majorant
+    {
+        public static void Main()
+        {
+           public static bool HasMajorant(int[] array, out int number)
+        {
+            number = 0;
+            int length = array.Length / 2;
+            for(int i = 0; i + length < array.Length; i++)
+            {
+                if(array[i] == array[i+length])
+                {
+                    number = array[i];
+                    return true;
+                }
+            }
+            return false;
+        }
+       public static void Main(string[] args)
+       {
+           int[] array = {3,4,4,4,5,6,4,8,4,4,3,4,5,4};
+           Array.Sort(array);
+           int number;
+           bool hasMajorant = HasMajorant(array, out number);
+           if(hasMajorant)
+           {
+               Console.WriteLine("The value '{0}' is majorant.", number);
+           }
+           else
+           {
+               Console.WriteLine("There is no majorant in the array.");
+           }
+           Console.WriteLine();
+       }          
+   }
+}
+
