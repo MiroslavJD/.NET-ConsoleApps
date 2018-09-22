68bbe88f09300c239d98c8e3ac0834a3e58a3603
Мирослав Димитров <33944218+MiroslavJD@users.noreply.github.com>
2018-09-22 07:42:15 +0300

    Add files via upload

---
+++ PascalTriangle!(2).cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiroslavD
+{
+    public class Program
+    {
+        // 33. Write a C# Sharp program to display by Pascal's triangle.
+
+
+        public static void Main(string[] args)
+        {
+            int c = 1;
+            Console.Write("Enter rows: ");
+            int rows = Int32.Parse(Console.ReadLine());
+            for(int i = 0; i < rows; i++)
+            {
+                for(int j = 1; j <= rows - i; j++)
+                    Console.Write(" ");
+                for(int k = 0; k<=i; k++)
+                {
+                    if (k == 0 || i == 0)
+                        c = 1;
+                    else
+                        c = c * (i - k + 1) / k;
+                    Console.Write("{0} ", c);
+                }
+                Console.WriteLine();          
+            }
+        }
+    }
+}
+   
+
+
+    
+
+   
+
+
+
+
+
+
+        
+
+
