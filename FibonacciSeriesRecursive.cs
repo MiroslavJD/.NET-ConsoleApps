0f4015ef92a3e368ec889948b1e9956d7e557a5e
Мирослав Димитров <33944218+MiroslavJD@users.noreply.github.com>
2018-09-23 01:04:18 +0300

    Add files via upload

---
+++ FibonacciSeriesRecursive.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Threading;
+using System.Collections;
+
+namespace MiroslavD
+{   // Strings
+    // Factorial recursive.
+ 
+    public class TestClass
+    {     
+        public static uint Factorial(uint n)
+        {
+               if(n == 0)
+               {
+                   return 1;
+                  
+               }
+               else
+               {
+                   return n * Factorial(n - 1);
+               }
+        }
+        public static void Main()
+        {
+            Console.Write("Enter number: ");
+            uint n = Convert.ToUInt32(Console.ReadLine());
+            Console.WriteLine("Factorial of {0} is {1}", n, Factorial(n));
+            Console.WriteLine();         
+        }
+    }
+}
+
+
+
+
