6072e370d9b4a071bc85a9c4a791664a558231a7
Мирослав Димитров <33944218+MiroslavJD@users.noreply.github.com>
2018-09-22 03:55:43 +0300

    Add files via upload

---
+++ OccurAndSortStringsByDictinaryKey.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace MiroslavD
+{
+    // Dictionary// 
+    class OccurAndSortStringsByDictinaryKey
+    {
+        private static readonly string TEXT = "Hello word. I am a developer. My name is Miroslav";
+
+        private static IDictionary<string, int> GetWordOccurrence(string text)
+        {
+            string[] tokens = text.Split(' ', '.', ',', '-', '?', '!');
+            IDictionary<string, int> words = new SortedDictionary<string, int>();
+            foreach (string word in tokens)
+            {
+                if (string.IsNullOrEmpty(text.Trim()))
+                {
+                    continue;
+                }
+                int count;
+                if (words.TryGetValue(word, out count))
+                {
+                    count = 0;
+                }
+                words[word] = count + 1;
+            }
+            return words;
+        }
+        private static void PrintWordOccurrenceCount(IDictionary<string, int> wordOccurrence)
+        {
+            foreach (KeyValuePair<string, int> wordEntry in wordOccurrence)
+            {
+                Console.WriteLine("Value {0} appears {1} time(s).", wordEntry.Key, wordEntry.Value);
+            }
+            Console.ReadKey();
+            Console.WriteLine();
+        }
+        public static void Main()
+        {
+            IDictionary<string, int> wordOccurrence = GetWordOccurrence(TEXT);
+            PrintWordOccurrenceCount(wordOccurrence);
+        }
+    }
+}
