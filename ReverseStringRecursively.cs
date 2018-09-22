7e95ba058d8a24367a6d767388aecf79a68ff8ac
Мирослав Димитров <33944218+MiroslavJD@users.noreply.github.com>
2018-09-23 00:20:15 +0300

    Add files via upload

---
+++ ReverseStringRecursively.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiroslavD
+{
+    class ReverseStringRecursively
+    {
+       public static string ReverseString(string str)
+        {
+           if(str.Length > 0)
+           {
+               return  str[str.Length-1] + ReverseString(str.Substring(0, str.Length-1));
+           }
+           else
+           {
+               return str;
+           }         
+        }
+       public static void Main()
+       {
+           Console.Write("String: ");
+           string str = Console.ReadLine();
+           Console.WriteLine("Reverse string: " + ReverseString(str));
+       }
+    }   
+}         
+
+
