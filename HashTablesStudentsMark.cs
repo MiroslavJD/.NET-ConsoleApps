c217b8dffa075a8f7243d7bb1cf7129efb3bb6ff
Мирослав Димитров <33944218+MiroslavJD@users.noreply.github.com>
2018-09-28 16:27:11 +0300

    Update HashTablesStudentsMark.cs

--- HashTablesStudentsMark.cs
+++ HashTablesStudentsMark.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MiroslavD
 {
-    class HashTablesStudentsMark
+    class HashSetTablesStudentsMark
     {     
         public static void Main()
         {
