b2fa1d660b0ec8143ad586d41c598d11d5aaa71c
Мирослав Димитров <33944218+MiroslavJD@users.noreply.github.com>
2018-10-31 15:44:39 +0200

    Add files via upload

---
+++ SayHelloToTheMaster.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiroslavD
+{
+    class Class
+    {
+        public static string Name(string name)
+        {
+            if(name == "Miroslav")
+            {
+               return "Say Hello!";
+            }
+            else
+            {
+                return "That's not the master";
+            }
+        }
+        public static void Main()
+        {
+
+            Console.Write("Enter name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine(Name(name));  
+        }
+    }
+}
